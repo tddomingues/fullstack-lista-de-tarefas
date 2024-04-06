65fdfa8f44416bbda4111d6eb9e26285de3a613f
tiago domingues <ttx8000@gmail.com>
2024-04-06 15:28:19 -0300

    configurando o banco de dados mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(require("./routes/routes"))
 
+require("./config/db.js")()
+
 app.listen(PORT, () => {
   console.log(`Servido funcionando na porta ${PORT}.`)
 })
