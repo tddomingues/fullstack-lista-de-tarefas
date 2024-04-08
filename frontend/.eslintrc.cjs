02546c7a2e31d7eca4a3d254f330fc48e4a52961
tiago domingues <ttx8000@gmail.com>
2024-04-07 21:14:03 -0300

    inserindo o eslint e prettier

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,16 +6,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  rules: {},
 }
